7dad68ce27afc20168ced4f25fa2978f76cdb355
irving-Cesar <irvingcesar22@gmail.com>
2022-06-02 19:51:26 -0300

    funcao para enviar dados a API

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const todo = {
@@ -20,10 +20,15 @@ function App() {
       time,
       done: false,
     };
-    
 
-    //Envio para a API
-    console.log(todo);
+    await fetch(API, "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "aplicattion/json",
+      },
+    });
+
     
     setTitle("");
     setTime("");
