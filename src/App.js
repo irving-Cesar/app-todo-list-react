12f77da34a9f345c6ff87bfbb57b0739dee1b90f
irving-Cesar <irvingcesar22@gmail.com>
2022-06-02 18:28:01 -0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+
 import { useState, useEffect } from 'react'
 import { BsTrash, BsBookMarkCheck, BsBookMarkCheckFill } from 'react-icons/bs'
 
+const API = "http://localhost:5000";
+
 function App() {
+  const [title, setTitle] = useState("");
+
   return (
     <div className="App">
       <h1>Todo</h1>
