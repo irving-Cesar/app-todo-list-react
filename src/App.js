94f378a91db10c8c7a722521de4e6c2771fe94c7
irving-Cesar <irvingcesar22@gmail.com>
2022-06-02 18:41:32 -0300

    add form

--- src/App.js
+++ src/App.js
@@ -11,9 +11,29 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviou!")
+  }
+
   return (
     <div className="App">
-      <h1>Todo</h1>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+
+      <div className="form-todo">
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="submit" value="enviar"></input>
+        </form>
+        <p>Formulário</p>
+      </div>
+
+      <div className="list-todo">
+        <h2>Lista de tarefas</h2>
+        {todos.length === 0 && <p>Não há tarefas</p>}
+      </div>
     </div>
   );
 }
